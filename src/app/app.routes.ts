5350f63def5c820958110afe484e1ca553b5b0dd
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-03-06 01:15:14 +0530

    routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {OrderComponent} from "./order/order.component";
+import {CustomerComponent} from "./customer/customer.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'customer',component:CustomerComponent},
+  {path:'about',component:AboutComponent},
+  {path:'order',component:OrderComponent},
+];
